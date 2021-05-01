834f534d3e607f87388fc6b37791b4475eb2b2e7
Biruk Kebede <biruk.key@gmail.com>
2021-05-01 16:36:34 +0100

    Deployed london-mini-guide frontend and backend to Heroku

--- challenge-london-mini-guide/server/index.js
+++ challenge-london-mini-guide/server/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const path = require('path');
+const Harrow = require('../data/Harrow.json');
+const Heathrow = require('../data/Heathrow.json');
+const Stratford = require('../data/Stratford.json');
+
 
 const PORT = process.env.PORT || 8000;
 
@@ -7,8 +11,16 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+const routes = [{
+  "/pharmacies": "returns pharmacies list for in a specific area",
+  "/colleges": "returns colleges list for in a specific area",
+  "/doctors": "returns doctors list for in a specific area",
+  "/hospitals": "returns hospitals list for in a specific area",
+  }];
+  
+
 app.get("/api", (req, res) => {
-  res.json({ message: "Server is ready!" });
+  res.json({message: 'Server is ready!'});
 });
 
 app.listen(PORT, () => {
